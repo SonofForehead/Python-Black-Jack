cdba7724d5400bbaed388933d9fd3cc85214d730
Son of Forehead <jpstoner@icloud.com>
2024-10-28 17:28:03 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+from random import shuffle
+
+cards = {"Ace": 11, "Two": 2, "Three": 3, "Four": 4, "Five": 5, "Six": 6, "Seven": 7, "Eight": 8, "Nine": 9, "Queen": 10,  "Jack": 11, "King": 12}
+category = ["♣️", "♦️", "♥️", "♠️"]
